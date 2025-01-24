03793a92d91957a28ab0ac6d39a89513d986687b
Rabiul Hasan Rafee <rhr277@gmail.com>
2025-01-24 23:19:49 +0600

    use of useState

--- src/Componenets/State/User.tsx
+++ src/Componenets/State/User.tsx
@@ -6,7 +6,8 @@ type AuthUser = {
 };
 
 export const User = () => {
-  const [user, setUser] = useState<AuthUser | null>(null);
+  //   const [user, setUser] = useState<AuthUser | null>(null);
+  const [user, setUser] = useState<AuthUser>({} as AuthUser);
 
   const handleLogin = () => {
     setUser({
@@ -15,18 +16,18 @@ export const User = () => {
     });
   };
 
-  const handleLogout = () => {
-    setUser(null);
-  };
+  //   const handleLogout = () => {
+  //     setUser(null);
+  //   };
 
   return (
     <div>
       <button className="btn" onClick={handleLogin}>
         Login
       </button>
-      <button className="btn" onClick={handleLogout}>
+      {/* <button className="btn" onClick={handleLogout}>
         Logout
-      </button>
+      </button> */}
       {user && (
         <>
           <div>User name is {user.name}</div>
